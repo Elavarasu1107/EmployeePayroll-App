efe80e21e548529a302d7f7550893fcf9818dff1
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-11 15:47:51 +0530

    [Elavarasu] UC4-Adding Gender,Department and Salary

---
+++ js/EmployeePayroll_JS.js
@@ -0,0 +1,6 @@
+//Salary Range
+const salary = document.querySelector('#salary');
+const message = document.querySelector('.salary-output');
+salary.addEventListener('input', function(){
+    message.textContent = salary.value;
+});
