bfd88087a597f8cc10ca266ff064ab9caf8de380
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-12 18:47:08 +0530

    [Elavarasu] UC10-Creating Employee Object

--- js/EmployeePayrollEvent_JS.js
+++ js/EmployeePayrollEvent_JS.js
@@ -35,3 +35,73 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         }
     });
 });
+
+const save = () =>{
+    try{
+        let employeePayroll = createEmployeePayroll();
+    }
+    catch (ex){
+        return;
+    }
+}
+
+const createEmployeePayroll = () =>{
+    let employeePayroll = new EmployeePayroll();
+    try{
+        employeePayroll.name = getInputValuesbyId('#name');
+    }
+    catch (ex){
+        setTextValue('.nameError', ex);
+        throw ex;
+    }
+    employeePayroll.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayroll.gender = getSelectedValues('[name=gender]').pop();
+    employeePayroll.department = getSelectedValues('[name=department]');
+    employeePayroll.salary = getInputValuesbyId('#salary');
+    employeePayroll.notes = getInputValuesbyId('#notes');
+    let date = getInputValuesbyId('#day') + " " + getInputValuesbyId('#month') + " " + getInputValuesbyId('#year');
+    employeePayroll.startDate = new Date(Date.parse(date)).toLocaleDateString();
+    alert(employeePayroll.toString());
+    return employeePayroll;
+}
+
+const getInputValuesbyId = (id) =>{
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getSelectedValues = (propertyValue) =>{
+    let allItems = document.querySelectorAll(propertyValue);
+    let selectedItems = [];
+    allItems.forEach(item =>{
+        if(item.checked)
+        {
+            selectedItems.push(item.value);
+        }
+    });
+    return selectedItems;
+}
+
+const resetForm = () =>{
+    setDefaultValue('#name','');
+    unCheckValues('[name = profile]');
+    unCheckValues('[name = gender]');
+    unCheckValues('[name = department]');
+    setDefaultValue('#salary','50000');
+    setDefaultValue('day','day');
+    setDefaultValue('month','month');
+    setDefaultValue('year','2020');
+    setDefaultValue('#notes','');
+}
+
+const setDefaultValue = (id, value) =>{
+    const item = document.querySelector(id)
+    item.textContent = value;
+}
+
+const unCheckValues = (propertyValue) =>{
+   let allItems = document.querySelectorAll(propertyValue);
+   allItems.forEach(item =>{
+    item.checked = false;
+   });
+}
