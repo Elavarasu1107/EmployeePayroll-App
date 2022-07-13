c01aec7b1d59259751cc25e96adb53da9057986a
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-13 16:10:42 +0530

    [Elavarasu] Creating JSON Object

--- js/HomePage_JS.js
+++ js/HomePage_JS.js
@@ -4,22 +4,55 @@ window.addEventListener('DOMContentLoaded', (event)=>{
 
 const createInnerHtml = () =>{
     const headerHtml = "<tr class='table-header'><th></th><th>Name</th><th>Gender</th><th>Salary</th><th>Department</th><th>Start Date</th><th>Actions</th></tr>";
+    let employeePayrollDate = createEmployeePayrollJSONObject()[0];
     const innerHtml = `${headerHtml}
         <tr>
-            <td><img src="../assets/Profile/employee-1.jpeg" alt="employee-1"></td>
-            <td>Elavarasu Appusamy</td>
-            <td>Male</td>
-            <td>50000</td>
+            <td><img src="${employeePayrollDate._profilePic}" alt=""></td>
+            <td>${employeePayrollDate._name}</td>
+            <td>${employeePayrollDate._gender}</td>
+            <td>${employeePayrollDate._salary}</td>
             <td>
-                <div class="department">Engineer</div>
-                <div class="department">Others</div>
+                <div class="department">${employeePayrollDate._department[0]}</div>
+                <div class="department">${employeePayrollDate._department[1]}</div>
             </td>
-            <td>10 May 2022</td>
+            <td>${employeePayrollDate._startDate}</td>
             <td class="actions">
-                <img id="1" onclick="remove(this)" alt="delete" src="../assets/delete.png" width="20px" height="20px">
-                <img id="1" onclick="update(this)" alt="update" src="../assets/pen.png" width="20px" height="20px">
+                <img id="${employeePayrollDate._id}" onclick="remove(this)" alt="delete" src="../assets/delete.png" width="20px" height="20px">
+                <img id="${employeePayrollDate._id}" onclick="update(this)" alt="update" src="../assets/pen.png" width="20px" height="20px">
             </td>
         </tr>
     `;
     document.querySelector('#table').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSONObject = () =>{
+    let employeePayrollListLocal = [
+        {
+            _name: 'Elavarasu',
+            _gender: 'Male',
+            _department:[
+                'Engineer',
+                'Others'
+            ],
+            _salary: '50000',
+            _startDate:'10 May 2022',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/Profile/employee-1.jpeg'
+        },
+        {
+            _name: 'Nantha Gopal',
+            _gender: 'Male',
+            _department:[
+                'Engineer',
+                'HR'
+            ],
+            _salary: '70000',
+            _startDate:'10 May 2022',
+            _note:'',
+            _id: new Date().getTime()+1,
+            _profilePic: '../assets/Profile/employee-4.jpeg'
+        }
+    ];
+    return employeePayrollListLocal;
 }
