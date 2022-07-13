b0de125b03f51ab722312fa7564af520e6d023fc
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-13 16:36:44 +0530

    [Elavarasu] Display Multiple Employees Data

--- js/HomePage_JS.js
+++ js/HomePage_JS.js
@@ -4,21 +4,25 @@ window.addEventListener('DOMContentLoaded', (event)=>{
 
 const createInnerHtml = () =>{
     const headerHtml = "<tr class='table-header'><th></th><th>Name</th><th>Gender</th><th>Salary</th><th>Department</th><th>Start Date</th><th>Actions</th></tr>";
-    let employeePayrollDate = createEmployeePayrollJSONObject()[0];
-    const innerHtml = `${headerHtml}
-        <tr>
-            <td><img src="${employeePayrollDate._profilePic}" alt=""></td>
-            <td>${employeePayrollDate._name}</td>
-            <td>${employeePayrollDate._gender}</td>
-            <td>${employeePayrollDate._salary}</td>
-            <td>${getDept(employeePayrollDate._department)}</td>
-            <td>${employeePayrollDate._startDate}</td>
-            <td class="actions">
-                <img id="${employeePayrollDate._id}" onclick="remove(this)" alt="delete" src="../assets/delete.png" width="20px" height="20px">
-                <img id="${employeePayrollDate._id}" onclick="update(this)" alt="update" src="../assets/pen.png" width="20px" height="20px">
-            </td>
-        </tr>
-    `;
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollList = createEmployeePayrollJSONObject();
+    for(const employeePayrollData of employeePayrollList)
+    {
+        innerHtml = `${innerHtml}
+            <tr>
+                <td><img src="${employeePayrollData._profilePic}" alt=""></td>
+                <td>${employeePayrollData._name}</td>
+                <td>${employeePayrollData._gender}</td>
+                <td>${employeePayrollData._salary}</td>
+                <td>${getDept(employeePayrollData._department)}</td>
+                <td>${employeePayrollData._startDate}</td>
+                <td class="actions">
+                    <img id="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/delete.png" width="20px" height="20px">
+                    <img id="${employeePayrollData._id}" onclick="update(this)" alt="update" src="../assets/pen.png" width="20px" height="20px">
+                </td>
+            </tr>
+        `;
+    }
     document.querySelector('#table').innerHTML = innerHtml;
 }
 
@@ -32,7 +36,7 @@ const createEmployeePayrollJSONObject = () =>{
                 'Others'
             ],
             _salary: '50000',
-            _startDate:'10 May 2022',
+            _startDate:'10 June 2022',
             _note:'',
             _id: new Date().getTime(),
             _profilePic: '../assets/Profile/employee-1.jpeg'
